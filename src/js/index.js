bca384808eb91902146c09d23b1a928636fdad23
Kevin Russell <71138132+krussell23@users.noreply.github.com>
2021-12-17 11:50:59 -0700

    heart for bridgette

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,5 @@ const app = document.querySelector("#app");
 
 // React JSX
 
-app.innerHTML = `<h1>TEST PAGE TO SHOW BRIDGETTE DEPLOYMENT</h1>`;
+app.innerHTML = `<h1>TEST PAGE TO SHOW BRIDGETTE DEPLOYMENT</h1>
+`;
